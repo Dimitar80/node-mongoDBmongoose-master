59c85931b55665ef5c427ca84e887b36feb55f0b
DIMITAR <47561857+Dimitar80@users.noreply.github.com>
2019-11-25 22:02:59 +0100

    node-mongoDB-loginItoken

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,13 +1,30 @@
 const mUsers = require('../models_/users');
 const vUsers = require('../validators/users');
 var validator = require('node-input-validator');
+var bcrypt = require('bcryptjs');
+const config = require('../config/index.js');
+var jwt =require('jsonwebtoken')
 
 const register = (req, res) => {
     var v = new validator.Validator(req.body, vUsers.createUser);
     v.check()
     .then(matched => {
         if(matched) {
-            return mUsers.createUser(req.body)
+             // Store hash in your password DB.
+            bcrypt.genSalt(10, function(err, salt) {
+                if(err){
+                    throw new Error(err);
+                    return
+                }
+                bcrypt.hash(req.body.password, salt, function(err, hash) {
+                    if(err){
+                        throw new Error(err);
+                    return
+                    }
+                    return mUsers.createUser({...req.body, password: hash});
+                });
+            });
+            
         } else {
             throw new Error('Validation failed');
         }
@@ -22,8 +39,34 @@ const register = (req, res) => {
 }
 
 const login = (req, res) => {
-    return res.status(200).send('ok');
+    console.log('pero');
+    mUsers.getUserPasswordByEmail(req.body.email)
+    .then((data) => {
+        bcrypt.compare(req.body.password, data.password, function(err, rez) {
+           if(err){
+               return res.status(500).send('Could not compare password');
+           }
+           if(rez){
+            //    return res.status(200).send('ok');
+            var tokenData = {
+                id: rez._id,
+                full_name: `${rez.first_name} ${rez.last_name}`,
+                email: rez.email
+            }
+            var token = jwt.sign(tokenData, config.getConfig('jwt').key);
+            return res.status(200).send({jwt: token});
+           }
+           return res.status(404).send('not found');
+        });
+        // console.log(data);
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).send('internal server error');
+    })
 }
+
+
 
 const renew = (req, res) => {
     return res.status(200).send('ok');
