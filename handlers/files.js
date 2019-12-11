e4469a45b669a176fb52d312374b4d6d8dd8f260
DIMITAR <47561857+Dimitar80@users.noreply.github.com>
2019-12-11 22:04:48 +0100

    node-mongoDB-upFileHashing

--- handlers/files.js
+++ handlers/files.js
@@ -1,6 +1,31 @@
+const randomString = require('randomstring');
+
 const UploadFile = (req, res) => {
   var file = req.files.file; //.file-poleto so ke go isprakjame//
-  file.mv('./uploads/slika.jpg', err => {
+
+  console.log(file);
+  if(file.size > 10 * 1024 * 1024){
+    return res.status(500).send('Filesize too big');
+  }
+
+  var allowedTypes = [
+      'image/png', 
+      'image/jpg', 
+      'image/jpeg', 
+      'image/jpeg', 
+      'image/gif' 
+  ];
+
+    if(allowedTypes.indexOf(file.mimetype) == -1){
+        return res.status(500).send('Filetype not on the list');
+    }
+
+    var prefix = randomString.generate({
+        lenght: 10,
+        charset: 'alphanumeric'
+    })
+
+  file.mv(`./uploads/${prefix}_${file.name}`, err => {
       if(err){
           console.log(err)
           return res.status(500).send('Internal Server Error');
