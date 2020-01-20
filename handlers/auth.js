e0238eed610df6d9bd71797e87202b52edf10e46
Dimitar80 <designcrafts.mk@gmail.com>
2020-01-20 18:31:44 +0100

    node-mongoDBmongoose-authS-01-20-2020

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,12 +1,11 @@
-const mUsers = require('../models_/users');
-const vUsers = require('../validators/users');
-var validator = require('node-input-validator');
-var bcrypt = require('bcryptjs');
-var jwt = require('jsonwebtoken');
-const config = require('../config/index.js');
-const randomstring = require('randomstring');
-const sgMail = require('@sendgrid/mail');
-
+const mUsers = require("../models_/users");
+const vUsers = require("../validators/users");
+var validator = require("node-input-validator");
+var bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+const config = require("../config/index.js");
+const randomstring = require("randomstring");
+const sgMail = require("@sendgrid/mail");
 
 // register - validator //
 
@@ -29,132 +28,133 @@ const sgMail = require('@sendgrid/mail');
 //     });
 // }
 
+// UGLAV!!! 01-20-2020 //
 // hashing //
 const register = (req, res) => {
-    var v = new validator.Validator(req.body, vUsers.createUser);
-    v.check()
+  var v = new validator.Validator(req.body, vUsers.createUser);
+  v.check()
     .then(matched => {
-        if(matched) {
-            // get user by email
-            return mUsers.getUserPasswordByEmail(req.body.email)
-            .then((ed) => {
-                console.log(ed);
-                if(!ed) {
-                    bcrypt.genSalt(10, function(err, salt) {
-                        if(err){
-                            throw new Error(err);
-                            return;
-                        }
-                        bcrypt.hash(req.body.password, salt, function(err, hash) {
-                            if(err){
-                                throw new Error(err);
-                                return;
-                            }
-                            var confirm_hash = randomstring.generate({
-                                length: 30,
-                                charset: 'alphanumeric'
-                            });
-                            mUsers.createUser({
-                                ...req.body, 
-                                password: hash,
-                                confirm_hash: confirm_hash,
-                                confirmed: false
-                            });
-                            sgMail.setApiKey(config.getConfig('mailer').key);
-                            const msg = {
-                                to: req.body.email,
-                                from: 'designcrafts.mk@gmail.com',
-                                subject: 'Thanks for registering',
-                                text: 'Thanks for registering',
-                                html: `<a href="http://localhost:8081/api/v1/confirm/${confirm_hash}">Click here to confirm your account</a>`,
-                            };
-                            sgMail.send(msg);
-                            return;
-                        });
-                    });
-                } else {
-                    throw new Error('Bad Request - User Exists');
+      if (matched) {
+        // get user by email
+        return mUsers
+          .getUserPasswordByEmail(req.body.email)
+          .then(ed => {
+            console.log(ed);
+            if (!ed) {
+              bcrypt.genSalt(10, function(err, salt) {
+                if (err) {
+                  throw new Error(err);
+                  return;
                 }
-            })
-            .catch(err => {
-                throw new Error(err);
-            });
-        } else {
-            throw new Error('Validation failed');
-        }
+                bcrypt.hash(req.body.password, salt, function(err, hash) {
+                  if (err) {
+                    throw new Error(err);
+                    return;
+                  }
+                  var confirm_hash = randomstring.generate({
+                    length: 30,
+                    charset: "alphanumeric"
+                  });
+                  mUsers.createUser({
+                    ...req.body,
+                    password: hash,
+                    confirm_hash: confirm_hash,
+                    confirmed: false
+                  });
+                  sgMail.setApiKey(config.getConfig("mailer").key);
+                  const msg = {
+                    to: req.body.email,
+                    from: "designcrafts.mk@gmail.com",
+                    subject: "Thanks for registering",
+                    text: "Thanks for registering",
+                    html: `<a href="http://localhost:8081/api/v1/confirm/${confirm_hash}">Click here to confirm your account</a>`
+                  };
+                  sgMail.send(msg);
+                  return;
+                });
+              });
+            } else {
+              throw new Error("Bad Request - User Exists");
+            }
+          })
+          .catch(err => {
+            throw new Error(err);
+          });
+      } else {
+        throw new Error("Validation failed");
+      }
     })
     .then(() => {
-        return res.status(201).send('ok');
+      return res.status(201).send("ok-new user is registered/created");
     })
     .catch(err => {
-        console.log(err);
-        return res.status(500).send(v.errors);
+      console.log(err);
+      return res.status(500).send(v.errors);
     });
-}
-
+};
 
 const login = (req, res) => {
-    mUsers.getUserPasswordByEmail(req.body.email)
-    .then((data) => {
-        bcrypt.compare(req.body.password, data.password, function(err, rez) {
-            if(err){
-                return res.status(500).send('Could not compare password');
-            }
-            if(rez){
-                var tokenData = {
-                    id: data._id,
-                    full_name: `${data.first_name} ${data.last_name}`,
-                    email: data.email
-                    // exp:
-                };
-                var token = jwt.sign(tokenData, config.getConfig('jwt').key);
-                return res.status(200).send({jwt: token});
-            }
-            return res.status(404).send('not found');
-        });
+  mUsers
+    .getUserPasswordByEmail(req.body.email)
+    .then(data => {
+      bcrypt.compare(req.body.password, data.password, function(err, rez) {
+        if (err) {
+          return res.status(500).send("Could not compare password");
+        }
+        if (rez) {
+          var tokenData = {
+            id: data._id,
+            full_name: `${data.first_name} ${data.last_name}`,
+            email: data.email
+            // exp:
+          };
+          var token = jwt.sign(tokenData, config.getConfig("jwt").key);
+          return res.status(200).send({ jwt: token });
+        }
+        return res.status(404).send("not found");
+      });
     })
     .catch(err => {
-        console.log(err);
-        return res.status(500).send('Could not get user');
+      console.log(err);
+      return res.status(500).send("Could not get user");
     });
 };
 
 const confirm = (req, res) => {
-    var hash = req.params.confirm_hash;
-    mUsers.confirmUserAccount(hash)
+  var hash = req.params.confirm_hash;
+  mUsers
+    .confirmUserAccount(hash)
     .then(() => {
-        return res.status(200).send('ok')
+      return res.status(200).send("ok");
     })
-    .catch((err) => {
-        return res.status(500).send('internal Server Error')
-    })
-}
-
-
+    .catch(err => {
+      return res.status(500).send("internal Server Error");
+    });
+};
 
 const renew = (req, res) => {
-    return res.status(200).send(req.user);
-    // return res.status(200).send('ok');
-}
+  return res.status(200).send(req.user);
+  // return res.status(200).send('ok');
+};
 
 const resetLink = (req, res) => {
-    return res.status(200).send('ok');
-}
+  return res.status(200).send("ok");
+};
 
 const resetPassword = (req, res) => {
-    return res.status(200).send('ok');
-}
+  return res.status(200).send("ok");
+};
 
 const changePassword = (req, res) => {
-    return res.status(200).send('ok');
-}
+  return res.status(200).send("ok");
+};
 
 module.exports = {
-    register,
-    login,
-    renew,
-    resetLink,
-    resetPassword,
-    changePassword,
-    confirm
-}
+  register,
+  login,
+  renew,
+  resetLink,
+  resetPassword,
+  changePassword,
+  confirm
+};
--- models_/users.js
+++ models_/users.js
@@ -1,74 +1,73 @@
-var mongoose = require('mongoose');
-
-
+var mongoose = require("mongoose");
 
 //Models//
 var User = mongoose.model(
-    'users',
-    new mongoose.Schema({
-        first_name: String,
-        last_name: String,
-        email: String,
-        password: String,
-        date_of_birth: Date,
-        telephone: String,
-        country: String,
-        // _created: Date,
-        // _modified: Date,
-        confirm_hash: String,
-        confirmed: Boolean
-       
+  "users",
+  new mongoose.Schema(
+    {
+      first_name: String,
+      last_name: String,
+      email: String,
+      password: String,
+      date_of_birth: Date,
+      telephone: String,
+      country: String,
+      _created: Date,
+      _modified: Date,
+      confirm_hash: String,
+      confirmed: Boolean
     } /*,
     {
         collection: 'user'
     }*/
-    )
+  )
 );
 
-const createUser = (data) => {
-    return new Promise((success, fail) => {
-        var user = new User(data);
-        user.save(/*data,*/ err =>{
-            if(err){
-                return fail(err);
-            }
-            return success();
-        });
-    });
-}
+// UGLAV!!! 01-20-2020 //
+const createUser = data => {
+  return new Promise((success, fail) => {
+    var user = new User(data);
+    user.save(
+      /*data,*/ err => {
+        if (err) {
+          return fail(err);
+        }
+        return success();
+      }
+    );
+  });
+};
 
 // getUserPasswordByEmail //
-const getUserPasswordByEmail = (email) => {
-    return new Promise((success, fail) => {  // 1 ili 0 - true ili false//
-        User.find({email: email}, {password: 1, email: 1, first_name:1, last_name: 1, }, (err, data) => {
-            if(err){
-                return fail(err);
-            }
-            return success(data[0]); //celiot "Prv" objekt od baza//
-        })
-    })
-}
+const getUserPasswordByEmail = email => {
+  return new Promise((success, fail) => {
+    // 1 ili 0 - true ili false//
+    User.find(
+      { email: email },
+      { password: 1, email: 1, first_name: 1, last_name: 1 },
+      (err, data) => {
+        if (err) {
+          return fail(err);
+        }
+        return success(data[0]); //celiot "Prv" objekt od baza//
+      }
+    );
+  });
+};
 
-const confirmUserAccount = (hash) => {
-    return new Promise((success, fail) => {
-        User.update({confirm_hash: hash}, {confirmed: true}, (err) => {
-            if(err){
-               return fail(err);
-            }
-            return success();
-        })
-    })
-}
-
-
-
-
-
-
-
+const confirmUserAccount = hash => {
+  return new Promise((success, fail) => {
+    User.update({ confirm_hash: hash }, { confirmed: true }, err => {
+      if (err) {
+        return fail(err);
+      }
+      return success();
+    });
+  });
+};
 
 module.exports = {
-    createUser,
-    getUserPasswordByEmail,
-    confirmUserAccount
-}
+  createUser,
+  getUserPasswordByEmail,
+  confirmUserAccount
+};
